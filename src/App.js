b3b6f5bc47e22f7ef605a2100b60bd7deac0bbc4
Muhammad Ilham Hafizha <75237342+hmfzh@users.noreply.github.com>
2021-04-08 14:58:31 +0700

    migration html css to react

--- src/App.js
+++ src/App.js
@@ -2,9 +2,45 @@ import "./styles.css";
 
 export default function App() {
   return (
-    <div className="App">
-      <h1>Hello CodeSandbox</h1>
-      <h2>Start editing to see some magic happen!</h2>
+    <div class="container">
+      <div class="frame">
+        <section class="header">
+          <button class="header-btn main-black-color">ADD</button>
+          <h1 class="header-title">Todo list</h1>
+          <button class="header-btn main-read-color">Claer</button>
+        </section>
+
+        <section class="add">
+          <form class="add-form">
+            <input type="text" class="add-input" />
+            <button class="add-btn">ADD</button>
+          </form>
+        </section>
+
+        <section class="todos">
+          <div class="todo">
+            <span class="todos-text">Learning React</span>
+          </div>
+          <div class="todo">
+            <span class="todos-text">Learning React</span>
+          </div>
+          <div class="todo">
+            <span class="todos-text">Learning React</span>
+          </div>
+          <div class="todo">
+            <span class="todos-text">Learning React</span>
+          </div>
+          <div class="todo">
+            <span class="todos-text">Learning React</span>
+          </div>
+          <div class="todo">
+            <span class="todos-text">Learning React</span>
+          </div>
+          <div class="todo">
+            <span class="todos-text">Learning React</span>
+          </div>
+        </section>
+      </div>
     </div>
   );
 }
