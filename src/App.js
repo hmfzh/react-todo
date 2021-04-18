d3032baf679ab47e9e52b2c96b89d11c5eb35273
Muhammad Ilham Hafizha <75237342+hmfzh@users.noreply.github.com>
2021-04-18 14:35:01 +0700

    Membuat Paper Component

--- src/App.js
+++ src/App.js
@@ -1,46 +1,44 @@
 import "./styles.css";
-
+import Paper from "./components/Paper";
 export default function App() {
   return (
-    <div class="container">
-      <div class="frame">
-        <section class="header">
-          <button class="header-btn main-black-color">ADD</button>
-          <h1 class="header-title">Todo list</h1>
-          <button class="header-btn main-read-color">Claer</button>
-        </section>
+    <Paper>
+      <section class="header">
+        <button class="header-btn main-black-color">ADD</button>
+        <h1 class="header-title">Todo list</h1>
+        <button class="header-btn main-read-color">Claer</button>
+      </section>
 
-        <section class="add">
-          <form class="add-form">
-            <input type="text" class="add-input" />
-            <button class="add-btn">ADD</button>
-          </form>
-        </section>
+      <section class="add">
+        <form class="add-form">
+          <input type="text" class="add-input" />
+          <button class="add-btn">ADD</button>
+        </form>
+      </section>
 
-        <section class="todos">
-          <div class="todo">
-            <span class="todos-text">Learning React</span>
-          </div>
-          <div class="todo">
-            <span class="todos-text">Learning React</span>
-          </div>
-          <div class="todo">
-            <span class="todos-text">Learning React</span>
-          </div>
-          <div class="todo">
-            <span class="todos-text">Learning React</span>
-          </div>
-          <div class="todo">
-            <span class="todos-text">Learning React</span>
-          </div>
-          <div class="todo">
-            <span class="todos-text">Learning React</span>
-          </div>
-          <div class="todo">
-            <span class="todos-text">Learning React</span>
-          </div>
-        </section>
-      </div>
-    </div>
+      <section class="todos">
+        <div class="todo">
+          <span class="todos-text">Learning React</span>
+        </div>
+        <div class="todo">
+          <span class="todos-text">Learning React</span>
+        </div>
+        <div class="todo">
+          <span class="todos-text">Learning React</span>
+        </div>
+        <div class="todo">
+          <span class="todos-text">Learning React</span>
+        </div>
+        <div class="todo">
+          <span class="todos-text">Learning React</span>
+        </div>
+        <div class="todo">
+          <span class="todos-text">Learning React</span>
+        </div>
+        <div class="todo">
+          <span class="todos-text">Learning React</span>
+        </div>
+      </section>
+    </Paper>
   );
 }
